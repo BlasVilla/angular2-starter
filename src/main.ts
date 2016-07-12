f966bd338c9a8c1f835e559f7992b525c0422aa7
Blas Villa <b.villa@loginvsi.com>
2016-07-12 16:35:01 +0200

    - added input components

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import {bootstrap} from '@angular/platform-browser-dynamic';
 import {enableProdMode} from '@angular/core';
 import {HTTP_PROVIDERS} from '@angular/http';
+import {provideForms, disableDeprecatedForms} from '@angular/forms';
 
 import {AppComponent, environment} from './app/';
 
@@ -8,4 +9,9 @@ if (environment.production) {
   enableProdMode();
 }
 
-bootstrap(AppComponent, [HTTP_PROVIDERS]);
+bootstrap(AppComponent, 
+  [
+    HTTP_PROVIDERS, 
+    disableDeprecatedForms(), 
+    provideForms()
+    ]);
